acd350eda9a99bcc6694cf58d73f57e762a4b508
Fabstero <faraya40788@ufide.ac.cr>
2025-07-06 12:09:20 -0600

    Funcionalidad de pago lista

--- src/main/resources/static/recursos/creaTablas.sql
+++ src/main/resources/static/recursos/creaTablas.sql
@@ -19,3 +19,14 @@ INSERT INTO usuario (nombre, apellido, correo, contrasena, ubicacion) VALUES
 
  <!-- Con esto se ven -->
 SELECT * FROM usuario;
+
+CREATE TABLE pago (
+    id_pago INT AUTO_INCREMENT PRIMARY KEY,
+    monto DECIMAL(10,2) NOT NULL,
+    fecha_pago DATETIME NOT NULL,
+    metodo_pago ENUM('TARJETA_CREDITO', 'TARJETA_DEBITO') NOT NULL
+);
+
+ <!-- Con esto se ven -->
+USE magicclothesdb;
+SELECT * FROM pago;
